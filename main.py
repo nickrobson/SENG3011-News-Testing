3875011a0c8c44940d9cc464f499792ac5bd02d9
Anna Azzam <annaazzam@live.com>
2017-04-27 14:51:10 +1000

    makes a csv file with timing results

--- main.py
+++ main.py
@@ -1,25 +1,57 @@
 #!/usr/bin/env python3
 
+import csv
+
 from datetime import datetime
 
 from testing.cba import CoolBananasAPI
 from testing.ffs import FFSAPI
 
 def main():
+    NUM_RUNS = 5
+
     cba = CoolBananasAPI()
     ffs = FFSAPI()
 
-    for test_case in test_cases:
-        printTest(test_case)
-        cbares = cba.query(*test_case)
-        ffsres = ffs.query(*test_case)
+    with open('timingResults.csv', 'w') as csvfile:
+        fieldnames = ['test_number', 'ffs_ave_time', 'cba_ave_time']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
-        print(cbares)
-        print()
-        print(ffsres)
-        print()
+        test_number = 0
+        for test_case in test_cases:
+            test_number += 1
+            printTest(test_case)
+            cbares = cba.query(*test_case)
+            ffsres = ffs.query(*test_case)
 
-        # TODO compare the responses
+            print(cbares)
+            print()
+            print(ffsres)
+            print()
+
+            ffs_ave = 0
+            cba_ave = 0
+            for i in range(1, NUM_RUNS):
+                cba_test_run = cba.query(*test_case)
+                ffs_test_run = ffs.query(*test_case)
+                if (cba_test_run.time):
+                    cba_ave = cba_ave + float(cba_test_run.time)
+                if (ffs_test_run.time):
+                    ffs_ave = ffs_ave + float(ffs_test_run.time.replace(" seconds", ""))
+            ffs_ave = ffs_ave / NUM_RUNS
+            cba_ave = ffs_ave / NUM_RUNS
+            if (ffs_ave == 0):
+                ffs_ave = "error" # when no timing results have been provided
+            if (cba_ave == 0):
+                cba_ave = "error"
+            
+
+            writer.writerow({'test_number': test_number,
+                'ffs_ave_time': ffs_ave,
+                'cba_ave_time': cba_ave})
+
+
 
 def printTest(test_case):
     print('RICs: ' + str(test_case[0]))
